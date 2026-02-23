7be3121074b2b9c1e76b194e265663c92d00e3ac
suryanandana <suryanandana85@gmail.com>
2026-02-23 09:54:15 +0800

    add analisis dan performance vercel

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
+<script setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
 <template>
   <div class="overflow-x-hidden">
     <NuxtLoadingIndicator color="#FFD700" />
+    <Analytics/>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
